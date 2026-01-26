b020f63ac1244ef4ddf6cd45f6c3ca46e2dd4ed6
Bryce Albertazzi <brycealbertazzi@gmail.com>
2026-01-26 12:09:58 -0700

    Initial Commit

---
+++ Trace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trace
+//
+//  Created by Bryce Albertazzi on 1/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trace/TraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraceApp.swift
+//  Trace
+//
+//  Created by Bryce Albertazzi on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct TraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
